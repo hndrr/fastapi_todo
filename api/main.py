ea4bc10348d816f63e4df2bc0623f6a81ba0359e
hndrr <yozhik20@gmail.com>
2023-06-07 21:04:15 +0900

    add: 起動時のconsole出力を追加

--- api/main.py
+++ api/main.py
@@ -3,6 +3,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.on_event("startup")
+async def startup_event():
+    print(
+        "INFO:     FastAPI running on http://localhost:8000/docs (Press CTRL+C to quit)"
+    )
+
+
 @app.get("/hello")
 async def hello():
     return {"message": "hello world!"}
