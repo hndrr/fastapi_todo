12df32fba436b5d923ec432243a35ec1f4c6b444
hndrr <yozhik20@gmail.com>
2023-06-14 02:57:44 +0900

    fix: Path_param, Query_param

--- api/main.py
+++ api/main.py
@@ -19,17 +19,11 @@ async def index():
 async def hello():
     return {"message": "hello world!"}
 
-# Path_param example
-# http://localhost:8000/countries/Japan
+# Path_param & Query_param example
+# http://localhost:8000/countries/Japan?city_name=Tokyo
 @app.get("/countries/{country_name}")
-async def country(country_name: str):
-    return {"country_name": country_name}
-
-# Query_param example
-# http://localhost:8000/countries?country_name=Japan
-@app.get("/countries")
-async def country(country_name: str = 'japan', country_id: str = 'jp'):
-    return {"country_name": country_name, "country_id": country_id}
+async def country(country_name: str = 'japan', city_name: str = 'Tokyo'):
+    return {"country_name": country_name, "city_name": city_name}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, access_log=True)
