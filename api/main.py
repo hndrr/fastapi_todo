8bcc028f87b13605cd2917cc888e1f027b171bd9
hndrr <yozhik20@gmail.com>
2023-06-14 03:11:57 +0900

    fix: 改行

--- api/main.py
+++ api/main.py
@@ -7,8 +7,9 @@ app = FastAPI()
 @app.on_event("startup")
 async def startup_event():
     print(
-        "INFO:     FastAPI running on http://localhost:8000/docs (Press CTRL+C to quit)"
+        "INFO: FastAPI running on http://localhost:8000/docs (CTRL+C to quit)"
     )
+
 
 @app.get("/")
 async def index():
@@ -19,11 +20,13 @@ async def index():
 async def hello():
     return {"message": "hello world!"}
 
+
 # Path_param & Query_param example
 # http://localhost:8000/countries/Japan?city_name=Tokyo
 @app.get("/countries/{country_name}")
 async def country(country_name: str = 'japan', city_name: str = 'Tokyo'):
     return {"country_name": country_name, "city_name": city_name}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, access_log=True)
